e85b3812092ab582ef2724233f7f2dc377da9025
a23fanro <a23fanro@hi>
2024-05-25 13:52:51 +0200

    Deleted textView with project name, changed layout for WebView so it takes up the whole screen, started working on internal About-page, added "myWebView.setVisibility(View.GONE);" in onCreate() to make the internal.html not appear before onOptionsItemSelected() is activated

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -31,19 +31,31 @@ import androidx.appcompat.widget.Toolbar;
 
 import java.util.concurrent.atomic.AtomicReference;
 
-@SuppressWarnings("FieldCanBeLocal")
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.widget.Toolbar;
+
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    public void showInternalWebPage(){
-        findViewById(R.id.minwebviewsID);
-        myWebView = findViewById(R.id.minwebviewsID);
-        myWebView.setWebViewClient(new WebViewClient()); // Do not open in Chrome!
-        myWebView.loadUrl("///android_asset/internal.html");
-    }
-
-    WebView myWebView;
+    private WebView myWebView;
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a23fanro";
-    private final String JSON_FILE = "tests.json";
     private final ArrayList<TestClass> testClasses = new ArrayList<>();
     private RecyclerViewAdapter adapter;
     private Gson gson;
@@ -55,15 +67,12 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //new JsonFile(this, this).execute(JSON_FILE);
+        myWebView = findViewById(R.id.minwebviewsID);
+        myWebView.setVisibility(View.GONE);
+
+        gson = new Gson();
+
         new JsonTask(this).execute(JSON_URL);
-        //testClasses.add(new TestClass("hej"));
-
-        //Log.d("heeeej", "HEJ" + testClasses.size());
-
-        //for (TestClass m : testClasses) {
-            //Log.d("ngt", "HEJ" + m.toString());
-        //}
 
         adapter = new RecyclerViewAdapter(this, testClasses, new RecyclerViewAdapter.OnClickListener() {
             @Override
@@ -71,31 +80,31 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
                 Toast.makeText(MainActivity.this, item.toString(), Toast.LENGTH_SHORT).show();
             }
         });
+
         RecyclerView view = findViewById(R.id.recycler_view);
         view.setLayoutManager(new LinearLayoutManager(this));
         view.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
     }
+
+    public void showInternalWebPage() {
+        myWebView.setVisibility(View.VISIBLE);  // Show WebView when displaying internal page
+        myWebView.setWebViewClient(new WebViewClient()); // Do not open in Chrome!
+        myWebView.loadUrl("file:///android_asset/internal.html");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-
         if (id == R.id.action_internal_web) {
-            Log.d("==>","Will display internal web page");
-
+            Log.d("==>", "Will display internal web page");
             showInternalWebPage();
-
             return true;
         }
 
@@ -105,22 +114,16 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     @SuppressLint("NotifyDataSetChanged")
     @Override
     public void onPostExecute(String json) {
+        Log.d("MainActivity", "Received JSON: " + json);
 
-        Log.d("MainActivity", "" + json);
-
-        Gson gson = new Gson();
-
-        Type type = new TypeToken<List<TestClass>>() {
-        }.getType();
+        Type type = new TypeToken<List<TestClass>>() {}.getType();
         ArrayList<TestClass> jsonTestClass = gson.fromJson(json, type);
         testClasses.addAll(jsonTestClass);
 
-        //mountains = gson.fromJson(json, type);
-
-
-        for (int i = 0; i < testClasses.size(); i++) {
-            Log.d("HEJ", testClasses.get(i).getName());
+        for (TestClass testClass : testClasses) {
+            Log.d("MainActivity", "Loaded item: " + testClass.getName());
         }
-        adapter.notifyDataSetChanged();
+
+        adapter.notifyDataSetChanged(); // Notify adapter of data changes
     }
 }
